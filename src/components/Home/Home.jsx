75e1305fbb7cb8b3c5e5dbaea968515be1c2e18c
Mmeso1 <anisiubammesoma8@gmail.com>
2025-02-12 01:34:38 +0000

    I decided to finish the Timbu-Cloud-Shop website. I just did the header section of the home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import "./Home.css";
+import "animate.css";
+import watch from "../../assets/home-header-watch.svg";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <section className="header">
+        <div className="texts">
+          <div className="txt">
+            <h1 className="main-text">
+              Your World of <span>Luxury Watch</span> at an Affordable Price
+            </h1>
+            <p className="sub-text">
+              Lorem ipsum dolor sit amet consectetur. Neque et risus
+              pellentesque dui praesent tempus erat a tincidunt. Quam diam
+              aliquet morbi cras venenatis.
+            </p>
+          </div>
+          <div className="cta-buttons">
+            <button>Shop Now</button>
+            <button>See Cart</button>
+          </div>
+        </div>
+        <img
+          className="header-img animate__animated animate__fadeIn"
+          src={watch}
+          alt="header watch image"
+        />
+      </section>
+    </>
+  );
 };
 
 export default Home;
